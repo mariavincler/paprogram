faeecb820e47f3a280f1cf3855d1012821e1e499
mariavincler <141173728+mariavincler@users.noreply.github.com>
2023-09-05 15:04:18 -0300

    Add files via upload

---
+++ maria.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numero = 10
+
+nome = "joao"
+
+booleano = True
+a=10
+b=5
+soma= a + b
+subtração= a - b
+multiplicação= a * b
+divisão= a/b
+
+
+va= True
+vb= False
+
+r1= va and vb
+r2= va or vb
+r3= not va
+
+
+c = 3
+r4 = c > 4
+r5 = c == 4
+r6 = != 5
+ r7 = c > 1 and nome == "joao"
+
+
