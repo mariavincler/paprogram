4b4beb9da38fd0e7b78eb6b12d38ffab5e74c23e
mariavincler <141173728+mariavincler@users.noreply.github.com>
2023-10-24 14:46:09 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,26 @@
+def soma (x,y):
+    return x + y
+
+print (soma (3,5))
+print (soma (7,9))
+print (soma (40,9))
+
+
+
+def simples(cor):
+    if cor == 'amarelo':
+        return 'escolheu certo'
+
+def médio (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return'tente outra cor'
+        
+def completo (cor):
+    if cor == 'rosa':
+        return 'escolheu certo'
+    elif cor == 'marrom' :
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
